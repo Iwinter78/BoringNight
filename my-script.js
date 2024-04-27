d729cc927b739edd50e1f44f11d41494af74462c
Iwinter78 <kille1404.so@gmail.com>
2024-04-27 22:07:14 +0200

    redid the code, was right before

--- my-script.js
+++ my-script.js
@@ -1,8 +1,6 @@
-window.onload = function() {
-    const createIframe = document.createElement('iframe');
-    createIframe.src = "https://www.youtube.com/embed/brwS_ZmVaRc";
-    createIframe.width = "560";
-    createIframe.height = "315";
-    createIframe.autoplay = "1";
-    document.body.appendChild(createIframe);
-}
+const createIframe = document.createElement('iframe');
+createIframe.src = "https://www.youtube.com/embed/brwS_ZmVaRc";
+createIframe.width = "560";
+createIframe.height = "315";
+createIframe.autoplay = "1";
+document.body.appendChild(createIframe);
