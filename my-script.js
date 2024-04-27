ff2fb1dcf26444ef01069f650b05a0d278f621a7
Iwinter78 <kille1404.so@gmail.com>
2024-04-27 21:58:09 +0200

    updated code

--- my-script.js
+++ my-script.js
@@ -1,6 +1,8 @@
-const createIframe = document.createElement('iframe');
-createIframe.src = "https://www.youtube.com/watch?v=brwS_ZmVaRc";
-createIframe.width = "560";
-createIframe.height = "315";
-createIframe.autoplay = "1";
-document.body.appendChild(createIframe);
+window.onload = function() {
+    const createIframe = document.createElement('iframe');
+    createIframe.src = "https://www.youtube.com/embed/brwS_ZmVaRc";
+    createIframe.width = "560";
+    createIframe.height = "315";
+    createIframe.autoplay = "1";
+    document.body.appendChild(createIframe);
+}
