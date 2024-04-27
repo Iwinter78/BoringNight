2dd566f8347a9de076368acc9dabd180a6ab0cd0
Iwinter78 <kille1404.so@gmail.com>
2024-04-27 21:50:52 +0200

    test code

---
+++ my-script.js
@@ -0,0 +1,6 @@
+const createIframe = document.createElement('iframe');
+createIframe.src = "https://www.youtube.com/watch?v=brwS_ZmVaRc";
+createIframe.width = "560";
+createIframe.height = "315";
+createIframe.autoplay = "1";
+document.body.appendChild(createIframe);
